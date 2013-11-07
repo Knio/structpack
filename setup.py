2576c68de20b59556a8b6118ff13b5c82e7c23d8
Tom Flanagan <tom@zkpq.ca>
2013-11-06 23:57:42 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@ import structpack
 from setuptools import setup
 
 setup(
-    name='structpack',
-    version=structpack.version,
-    url='http://github.com/Knio/structpack',
-    description='''A Python library for serializing and deserializing object trees to JSON-compatable values (dicts, lists, strings, ints, floats, bools).''',
-    author='Tom Flanagan',
-    author_email='tom@zkpq.ca',
-    license='MIT',
+    name        = 'structpack',
+    version     = structpack.__version__,
+    url         = 'https://github.com/Knio/structpack',
+    author      = 'Tom Flanagan',
+    author_email= 'tom@zkpq.ca',
+    description = '''A Python library for serializing and deserializing object trees to JSON-compatable values (dicts, lists, strings, ints, floats, bools).''',
+    long_description = open('README.md').read(),
+    license = 'MIT',
     packages=['structpack'],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
 )
